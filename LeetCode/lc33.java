749ad4bcd5104258092a02ce2d1404c96140ef1d
Pravallika Nakarikanti <52459116+pravallika-1305@users.noreply.github.com>
2022-02-20 10:13:39 +0530

    microsoft frequently asked models

---
+++ LeetCode/lc33.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = (low + high) >> 1;
+            //check if the element is found or not
+            if(nums[mid] == target){
+                return mid;
+            }
+            //check if target lies in left half
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] >= target){
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            //check if target lies in right half
+            } else{
+                if(nums[mid] <= target && nums[high] >= target){
+                    low = mid + 1;
+                } else{
+                    high = mid - 1;
+                }
+            }
+            
+        }
+        return -1;
+        
+    }
+}
