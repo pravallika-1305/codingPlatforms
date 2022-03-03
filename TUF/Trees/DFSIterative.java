0614265da4186ab85288d12c65550d648b3ccf32
Pravallika Nakarikanti <52459116+pravallika-1305@users.noreply.github.com>
2022-03-03 20:46:38 +0530

    iterative preorder traversal
    using stacks

---
+++ TUF/Trees/DFSIterative.java
@@ -0,0 +1,27 @@
+public class Node{
+  int value;
+  Node left;
+  Node right;
+  public Node(){}
+  public Node(int key){
+    this.value = key;
+  }
+  public Node(int key, Node leftVal, Node rightVal){
+    this.value = key;
+    this.left = leftVal;
+    this.right = rightVal;
+  }
+
+public List<Integer> preorderIterative(Node root){
+  List<Integer> preorder = new ArrayList<Integer>();
+  if(root == null) return preorder;
+  Stack<Node> st = new Stack<Node>();
+  st.push(root);
+  while(!st.isEmpty()){
+    root = st.pop();
+    preorder.add(root.value);
+    if (root.right != null) st.push(root.right);
+    if (root.left != null) st.push(root.left);
+   }
+  return preorder;
+}
