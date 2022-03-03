feffb4a6f7df30e5d02e195373078fd1cfb85869
Pravallika Nakarikanti <52459116+pravallika-1305@users.noreply.github.com>
2022-03-03 20:27:12 +0530

    level-order traversal of trees using queues

---
+++ TUF/Trees/BFSTraversal.java
@@ -0,0 +1,33 @@
+public class Node{
+  int value;
+  Node left;
+  Node right;
+  public Node(){}
+  public Node(int key){
+    this.value = key;
+  }
+  public Node(int key, Node leftVal, Node rightVal){
+    this.value = key;
+    this.left = leftVal;
+    this.right = rightVal;
+  }
+
+public List<List<Integer>> BFSTraversal(Node root){
+  Queue<Node> queue = new LinkedList<Node>();
+  List<List<Integer>> wrapList = new LinkedList<List<Integer>();
+  if(root === null) return wrapList;
+  queue.offer(root);
+  while(!queue.isEmpty()){
+    int numbersInTheLevel = queue.size();
+    List<Integer> levelList = new LinkedList<Integer>();
+    for(int i = 0; i < numbersInTheLevel; i++){
+      if (queue.peek().left != null) queue.offer(queue.peek().left);
+      if (queue.peek().right != null) queue.offer(queue.peek().right);
+    }
+      wrapList.add(levelList);
+   }
+  return wrapList;
+}
+    
+      
+    
