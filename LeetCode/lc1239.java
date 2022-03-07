5f43dbaa35cafc615d854f939fb8b49a2c1bee38
Pravallika Nakarikanti <52459116+pravallika-1305@users.noreply.github.com>
2022-03-07 08:24:22 +0530

    Create lc1239.java

---
+++ LeetCode/lc1239.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int result = 0;
+    
+    public int maxLength(List<String> arr) {
+        if (arr == null || arr.size() == 0)    
+            return 0;
+        dfs(arr, "", 0);
+        return result;
+    }
+        
+        
+        
+    //finding all possible combinations of the string to reach at a valid string
+    public void dfs(List<String> arr, String path, int idx) {
+        boolean isUniqueChar = isUniqueChars(path);
+
+        if (isUniqueChar) {
+          result = Math.max(path.length(), result);
+        }
+
+        if (idx == arr.size() || !isUniqueChar) {
+          return;
+        }
+
+        for (int i = idx; i < arr.size(); i++) {
+          dfs(arr, path + arr.get(i), i + 1);
+        }
+
+      }
+    
+    //checking whether the string combination is valid/unique or not
+    private boolean isUniqueChars(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+          if (set.contains(c)) {
+            return false;
+          }
+          set.add(c);
+        }
+        return true;
+    }
+
+
+        
+}
